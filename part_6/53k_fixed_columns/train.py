0a4e511b237c0027352a8b5542ece56f6cb53512
Sukhrob Yangibaev <metamorphis64@gmail.com>
2024-02-19 15:48:52 +0500

    revert all decision tree models

--- part_6/53k_fixed_columns/train.py
+++ part_6/53k_fixed_columns/train.py
@@ -24,37 +24,41 @@ X_train, X_test, y_train, y_test = train_test_split(
 results = []
 
 # --------------------------------
-# criterion = 'gini'
+criterion = 'gini'
 
-# gini_classifier = DecisionTreeClassifier(criterion=criterion, random_state=1)
-# gini_classifier.fit(X_train, y_train)
-# y_pred = gini_classifier.predict(X_test)
+gini_classifier = DecisionTreeClassifier(criterion=criterion, random_state=1)
+gini_classifier.fit(X_train, y_train)
+y_pred = gini_classifier.predict(X_test)
 
-# acc = accuracy_score(y_test, y_pred)
-# acc_avg += acc
-# print('CART', acc)
-# results.append({
-#     'classifier': 'CART',
-#     'criterion': criterion,
-#     'accuracy_score': acc
-# })
+acc = accuracy_score(y_test, y_pred)
+print('CART', acc)
+results.append({
+    'classifier': 'CART',
+    'criterion': criterion,
+    'accuracy_score': acc
+})
+
+with open('part_6/53k_fixed_columns/cart_classifier.pkl', 'wb') as f:
+    pickle.dump(gini_classifier, f)
 
 # --------------------------------
-# criterion = 'entropy'
+criterion = 'entropy'
 
-# entropy_classifier = DecisionTreeClassifier(
-#     criterion=criterion, random_state=1)
-# entropy_classifier.fit(X_train, y_train)
-# y_pred = entropy_classifier.predict(X_test)
+entropy_classifier = DecisionTreeClassifier(
+    criterion=criterion, random_state=1)
+entropy_classifier.fit(X_train, y_train)
+y_pred = entropy_classifier.predict(X_test)
 
-# acc = accuracy_score(y_test, y_pred)
-# acc_avg += acc
-# print('C4.5', acc)
-# results.append({
-#     'classifier': 'C4.5',
-#     'criterion': criterion,
-#     'accuracy_score': acc
-# })
+acc = accuracy_score(y_test, y_pred)
+print('C4.5', acc)
+results.append({
+    'classifier': 'C4.5',
+    'criterion': criterion,
+    'accuracy_score': acc
+})
+
+with open('part_6/53k_fixed_columns/c4.5_classifier.pkl', 'wb') as f:
+    pickle.dump(entropy_classifier, f)
 
 # --------------------------------
 etc_results = []
@@ -77,51 +81,49 @@ with open('part_6/53k_fixed_columns/et_classifier.pkl', 'wb') as f:
     pickle.dump(et_classifier, f)
 
 # --------------------------------
-# gb_results = []
+gb_results = []
 
-# gb_classifier = GradientBoostingClassifier(
-#     loss='log_loss', n_estimators=50, learning_rate=1, criterion='friedman_mse', max_depth=4, random_state=1)
-# gb_classifier.fit(X_train, y_train)
-# y_pred = gb_classifier.predict(X_test)
-# gb_results.append({
-#     'loss': 'log_loss',
-#     'learning_rate': 1,
-#     'n_estimators': 50,
-#     'criterion': 'friedman_mse',
-#     'max_depth': 4,
-#     'accuracy_score': accuracy_score(y_test, y_pred)
-# })
+gb_classifier = GradientBoostingClassifier(
+    loss='log_loss', n_estimators=50, learning_rate=1, criterion='friedman_mse', max_depth=4, random_state=1)
+gb_classifier.fit(X_train, y_train)
+y_pred = gb_classifier.predict(X_test)
+gb_results.append({
+    'loss': 'log_loss',
+    'learning_rate': 1,
+    'n_estimators': 50,
+    'criterion': 'friedman_mse',
+    'max_depth': 4,
+    'accuracy_score': accuracy_score(y_test, y_pred)
+})
 
-# result = max(gb_results, key=lambda x: x['accuracy_score'])
-# acc_avg += result['accuracy_score']
-# print('Gradient Boosting', result['accuracy_score'])
-# result['classifier'] = 'Gradient Boosting'
-# results.append(result)
+result = max(gb_results, key=lambda x: x['accuracy_score'])
+print('Gradient Boosting', result['accuracy_score'])
+result['classifier'] = 'Gradient Boosting'
+results.append(result)
 
-# with open('part_6/53k_xp_per_min/gb_classifier.pkl', 'wb') as f:
-#     pickle.dump(gb_classifier, f)
+with open('part_6/53k_fixed_columns/gb_classifier.pkl', 'wb') as f:
+    pickle.dump(gb_classifier, f)
 
 # --------------------------------
-# hgb_results = []
+hgb_results = []
 
-# hgb_classifier = HistGradientBoostingClassifier(
-#     learning_rate=0.2, max_iter=100, random_state=1)
-# hgb_classifier.fit(X_train, y_train)
-# y_pred = hgb_classifier.predict(X_test)
-# hgb_results.append({
-#     'learning_rate': 0.2,
-#     'max_iter': 100,
-#     'accuracy_score': accuracy_score(y_test, y_pred)
-# })
+hgb_classifier = HistGradientBoostingClassifier(
+    learning_rate=0.2, max_iter=100, random_state=1)
+hgb_classifier.fit(X_train, y_train)
+y_pred = hgb_classifier.predict(X_test)
+hgb_results.append({
+    'learning_rate': 0.2,
+    'max_iter': 100,
+    'accuracy_score': accuracy_score(y_test, y_pred)
+})
 
-# result = max(hgb_results, key=lambda x: x['accuracy_score'])
-# acc_avg += result['accuracy_score']
-# print('Hist Gradient Boosting', result['accuracy_score'])
-# result['classifier'] = 'Hist Gradient Boosting'
-# results.append(result)
+result = max(hgb_results, key=lambda x: x['accuracy_score'])
+print('Hist Gradient Boosting', result['accuracy_score'])
+result['classifier'] = 'Hist Gradient Boosting'
+results.append(result)
 
-# with open('part_6/53k_xp_per_min/hgb_classifier.pkl', 'wb') as f:
-#     pickle.dump(hgb_classifier, f)
+with open('part_6/53k_fixed_columns/hgb_classifier.pkl', 'wb') as f:
+    pickle.dump(hgb_classifier, f)
 
 # --------------------------------
 rf_results = []
@@ -145,24 +147,23 @@ with open('part_6/53k_fixed_columns/rf_classifier.pkl', 'wb') as f:
     pickle.dump(rf_classifier, f)
 
 # --------------------------------
-# ada_results = []
+ada_results = []
 
-# ab_classifier = AdaBoostClassifier(
-#     n_estimators=50, learning_rate=0.1, algorithm='SAMME', random_state=0)
-# ab_classifier.fit(X_train, y_train)
-# y_pred = ab_classifier.predict(X_test)
-# ada_results.append({
-#     'n_estimators': 50,
-#     'learning_rate': 0.1,
-#     'algorithm': 'SAMME.R',
-#     'accuracy_score': accuracy_score(y_test, y_pred)
-# })
+ab_classifier = AdaBoostClassifier(
+    n_estimators=50, learning_rate=0.1, algorithm='SAMME', random_state=0)
+ab_classifier.fit(X_train, y_train)
+y_pred = ab_classifier.predict(X_test)
+ada_results.append({
+    'n_estimators': 50,
+    'learning_rate': 0.1,
+    'algorithm': 'SAMME.R',
+    'accuracy_score': accuracy_score(y_test, y_pred)
+})
 
-# result = max(ada_results, key=lambda x: x['accuracy_score'])
-# print('Adaboost', result['accuracy_score'])
-# result['classifier'] = 'Adaboost'
-# results.append(result)
+result = max(ada_results, key=lambda x: x['accuracy_score'])
+print('Adaboost', result['accuracy_score'])
+result['classifier'] = 'Adaboost'
+results.append(result)
 
-# with open('part_6/53k_xp_per_min/ab_classifier.pkl', 'wb') as f:
-#     pickle.dump(ab_classifier, f)
-
+with open('part_6/53k_fixed_columns/ab_classifier.pkl', 'wb') as f:
+    pickle.dump(ab_classifier, f)
