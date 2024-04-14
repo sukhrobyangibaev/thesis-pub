676a563b43b6d9e9674396560531d8c05750b07d
Sukhrob Yangibaev <metamorphis64@gmail.com>
2024-04-14 08:54:04 +0500

    fix hero embedding

--- part_9_embedding/heroes/train_winners.py
+++ part_9_embedding/heroes/train_winners.py
@@ -9,21 +9,25 @@ MONGO_CLIENT = pymongo.MongoClient("mongodb://192.168.1.7:27017/")
 SDA_DB = MONGO_CLIENT["steam_dota_api"]
 LGC_COL = SDA_DB["league_games_col"]
 
+LGC_COL.create_index("match_id")
+
 unique_match_ids = LGC_COL.distinct("match_id")
 print('Number of unique matches:', len(unique_match_ids))
+
+# Fetch all matches at once based on unique_match_ids
+matches_cursor = LGC_COL.find({"match_id": {"$in": unique_match_ids}})
 
 matchups = []
 
 print("Processing matches...")
-for match_id in unique_match_ids:
-    match = LGC_COL.find_one({"match_id": match_id})
+for match in matches_cursor:
     matchup = []
 
     if match['winner'] == 'radiant':
-        for i, player in enumerate(match["scoreboard"]["radiant"]["players"]):
+        for player in match["scoreboard"]["radiant"]["players"]:
             matchup.append(str(player["hero_id"]))
     else:
-        for i, player in enumerate(match["scoreboard"]["dire"]["players"]):
+        for player in match["scoreboard"]["dire"]["players"]:
             matchup.append(str(player["hero_id"]))
 
     matchups.append(matchup)
